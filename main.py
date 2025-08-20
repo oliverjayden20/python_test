171ece4ac184d003a334ef3bfb8f94f5044b5c38
oliverjayden20 <oliverjayden93@gmail.com>
2025-08-20 16:14:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def in_autotests_we_trust(a, b):
     if a == b:
-        print('Test passed')
+        print('boy this boring time to make some real code!!')
     else:
         print('Test failed')
 
